790448968796a21cea3a2ed5e81b6c2593422045
wezads wezads <wezadswezads@gmail.com>
2026-03-31 07:40:36 +0000

    i have Renamed the tab name in spreadsheet from  Flight Enquiries to Air

--- src/app/air-tickets/actions.ts
+++ src/app/air-tickets/actions.ts
@@ -1,3 +1,4 @@
+
 'use server';
 
 import { z } from 'zod';
@@ -56,7 +57,7 @@ export async function submitAirTicketRequest(values: z.infer<typeof airTicketFor
         'Travel Class': values.travelClass,
         Message: values.message,
         Consent: values.consent,
-    }, process.env.TAB_AIR_TICKETS || 'Air Ticket');
+    }, process.env.TAB_AIR_TICKETS || 'Air Ticket Enquiries');
     return { success: true, message: 'Your request has been sent successfully!' };
   } catch (error) {
      console.error(error);
